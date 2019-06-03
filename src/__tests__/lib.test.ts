2989ea47146be9b2114a043822e369c99f673bcd
envis10n <envis10n@protonmail.com>
2019-06-02 19:56:26 -0500

    Fix endless loop

--- src/__tests__/lib.test.ts
+++ src/__tests__/lib.test.ts
@@ -2,10 +2,10 @@ import arguets from "../lib";
 
 test("arguets", () => {
     const options: arguets.IOptionDef[] = [
-        {name: "test", alias: "t", type: "boolean"},
-        {name: "file", alias: "f", type: "string"},
+        { name: "test", alias: "t", type: "boolean" },
+        { name: "file", alias: "f", type: "string" },
     ];
-    const args: string[] = "testing args parsing -t true".split(" ");
+    const args: string[] = "testing args parsing -t true --help".split(" ");
     const parsed: arguets.IArguments = arguets(args, options);
     expect(parsed.options.test).toBe(true);
     expect(parsed.options.file).toBe(undefined);
--- src/lib.ts
+++ src/lib.ts
@@ -7,10 +7,18 @@ function getEnclosed(args: string[]): string[] {
     let buffer: string[] = [];
     let symbol: string = "";
     for (const arg of args) {
-        if ((arg[0] === "\"" || arg[0] === "'") && arg.length > 1 && buffer.length === 0) {
+        if (
+            (arg[0] === '"' || arg[0] === "'") &&
+            arg.length > 1 &&
+            buffer.length === 0
+        ) {
             symbol = arg[0];
             buffer.push(arg.substring(1));
-        } else if (arg[arg.length - 1] === symbol && arg.length > 1 && buffer.length !== 0) {
+        } else if (
+            arg[arg.length - 1] === symbol &&
+            arg.length > 1 &&
+            buffer.length !== 0
+        ) {
             buffer.push(arg.substring(0, arg.length - 1));
             symbol = "";
             res.push(buffer.join(" "));
@@ -29,69 +37,77 @@ function getEnclosed(args: string[]): string[] {
  * @param args The argument array to parse.
  * @param defs An array of option definitions.
  */
-function getOptions(args: string[], defs: ArgueTS.IOptionDef[] = []): ArgueTS.ICLIOptionObj {
+function getOptions(
+    args: string[],
+    defs: ArgueTS.IOptionDef[] = []
+): ArgueTS.ICLIOptionObj {
     const reg1: RegExp = new RegExp(/\-\-?(\w+)/);
     const res: ArgueTS.ICLIOptionObj = {};
     let i: number = args.findIndex((e) => reg1.test(e));
     while (i !== -1) {
         const defI: number = defs.findIndex(
-            (e) => `--${e.name}` === args[i] || `-${e.alias}` === args[i],
+            (e) => `--${e.name}` === args[i] || `-${e.alias}` === args[i]
         );
         if (defI !== -1) {
             if (defI > 0) {
                 defs.splice(defI, 1);
             }
             const def: ArgueTS.IOptionDef = defs[defI];
-            if (res[def.name] === undefined) {
-                if (def.switch) {
-                    args.splice(i, 1);
-                    res[def.name] = true;
-                } else {
-                    if (args[i + 1] !== undefined) {
-                        let val: any = args[i + 1];
-                        switch (def.type) {
-                            case "boolean":
-                                if (val === "true") {
-                                    val = true;
-                                } else {
-                                    val = false;
-                                }
-                                break;
-                            case "json":
-                                try {
-                                    val = JSON.parse(val);
-                                } catch (e) {
-                                    val = null;
-                                }
-                                break;
-                            case "string":
-                                val = val;
-                                break;
-                            case "number":
-                                if (!isNaN(val)) {
-                                    val = Number(val);
-                                } else {
-                                    val = 0;
-                                }
-                                break;
-                            default:
-
-                                break;
-                        }
-                        res[def.name] = val;
-                        args.splice(i, 2);
-                    } else {
-                        res[def.name] = null;
+            if (def !== undefined) {
+                if (res[def.name] === undefined) {
+                    if (def.switch) {
                         args.splice(i, 1);
+                        res[def.name] = true;
+                    } else {
+                        if (args[i + 1] !== undefined) {
+                            let val: any = args[i + 1];
+                            switch (def.type) {
+                                case "boolean":
+                                    if (val === "true") {
+                                        val = true;
+                                    } else {
+                                        val = false;
+                                    }
+                                    break;
+                                case "json":
+                                    try {
+                                        val = JSON.parse(val);
+                                    } catch (e) {
+                                        val = null;
+                                    }
+                                    break;
+                                case "string":
+                                    val = val;
+                                    break;
+                                case "number":
+                                    if (!isNaN(val)) {
+                                        val = Number(val);
+                                    } else {
+                                        val = 0;
+                                    }
+                                    break;
+                                default:
+                                    break;
+                            }
+                            res[def.name] = val;
+                            args.splice(i, 2);
+                        } else {
+                            res[def.name] = null;
+                            args.splice(i, 1);
+                        }
+                    }
+                } else {
+                    if (def.switch) {
+                        args.splice(i, 1);
+                    } else {
+                        args.splice(i, 2);
                     }
                 }
             } else {
-                if (def.switch) {
-                    args.splice(i, 1);
-                } else {
-                    args.splice(i, 2);
-                }
+                args.splice(i, 1);
             }
+        } else {
+            args.splice(i, 1);
         }
         i = args.findIndex((e) => reg1.test(e));
     }
@@ -109,7 +125,10 @@ function getOptions(args: string[], defs: ArgueTS.IOptionDef[] = []): ArgueTS.IC
  * @param {string[]} args Arguments array. Default: process.argv.slice(2)
  * @param {ArgueTS.IOptionDef[]} defs Option definition array. Default: undefined.
  */
-function ArgueTS(args: string[] = process.argv.slice(2), defs?: ArgueTS.IOptionDef[]): ArgueTS.IArguments {
+function ArgueTS(
+    args: string[] = process.argv.slice(2),
+    defs?: ArgueTS.IOptionDef[]
+): ArgueTS.IArguments {
     args = getEnclosed(args);
     const options: ArgueTS.ICLIOptionObj = getOptions(args, defs);
     return {
